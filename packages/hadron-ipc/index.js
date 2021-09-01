8a9f3901da0e36705b8e646b7e5694163374d3c4
Michael Rose <michael_rose@gmx.de>
2021-09-01 11:08:47 +0200

    eliminate usages of dataService.model across compass

--- packages/hadron-ipc/index.js
+++ packages/hadron-ipc/index.js
@@ -2,14 +2,19 @@
 
 const isRenderer = require('is-electron-renderer');
 
-if (isRenderer) {
-  module.exports = require('./lib/renderer');
-} else if (typeof require('electron') !== 'string') {
-  module.exports = require('./lib/main');
-} else {
-  if (process.env.NODE_ENV !== 'test') {
-    // eslint-disable-next-line no-console
-    console.warn('Unsupported environment for hadron-ipc');
+const isElectron = typeof require('electron') !== 'string';
+
+function requireExportModule() {
+  if (!isElectron) {
+    if (process.env.NODE_ENV !== 'test') {
+      // eslint-disable-next-line no-console
+      console.warn('Unsupported environment for hadron-ipc');
+    }
+
+    return {};
   }
-  module.exports = {};
+
+  return isRenderer ? require('./lib/renderer') : require('./lib/main');
 }
+
+module.exports = requireExportModule();
